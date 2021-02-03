0fbf5505bedd51d5f0974426aacfbb469b9c39c6
bhqmz <791019127@qq.com>
2021-02-03 17:19:24 +0800

    *初步完成事件分发功能

--- Assets/Script/Core/EventDispatcher.cs
+++ Assets/Script/Core/EventDispatcher.cs
@@ -1,9 +1,39 @@
 ﻿
 using System;
+using System.Collections.Generic;
 
 public class EventDispatcher
 {
-    public Action<string, object> TestChang;
+    private Dictionary<string, Action<object>> events = new Dictionary<string, Action<object>>();
 
+    public Dictionary<string, Action<object>> EventList
+    {
+        get
+        {
+            return events;
+        }
+    }
 
+    protected void Event(string type, object data)
+    {
+        events[type]?.Invoke(data);
+    }
+
+    public void On(string type, Action<object> action)
+    {
+        //防止重复添加
+        Off(type, action);
+
+        events[type] += action;
+    }
+
+    public void Once(string type, Action<object> action)
+    {
+        On(type, action);
+    }
+
+    public void Off(string type, Action<object> action)
+    {
+        events[type] -= action;
+    }
 }
